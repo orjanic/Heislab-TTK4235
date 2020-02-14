3dc5d920fadc7bc8b13e8e245d386844c52c0f9f
Krnil <krnil@stud.ntnu.no>
2020-02-14 11:56:12 +0100

    order started

--- source/order.c
+++ source/order.c
@@ -1,1 +1,13 @@
 #include "order.h"
+
+
+void order_update_list(){
+    for (int ot=0; ot<HARDWARE_NUMBER_OF_ORDER_TYPES;ot++){
+        for (int f=0;f<HARDWARE_NUMBER_OF_FLOORS; f++){
+            if (hardware_read_order(f, (HardwareOrder)ot)) {
+                (order_list[ot][f]).order_active=1;
+                hardware_command_order_light(f,(HardwareOrder)ot,1);
+            }
+        }
+    }
+}
--- source/order.h
+++ source/order.h
@@ -1,9 +1,17 @@
 #ifndef ORDER_H
 #define ORDER_H
 
-//#include "hardware.h"
+#include "hardware.h"
 
+typedef struct {
+    HardwareOrder order_type;
+    HardwareFloor order_floor;
+    int order_active;
+} Order;
 
+static Order order_list[HARDWARE_NUMBER_OF_ORDER_TYPES][HARDWARE_NUMBER_OF_FLOORS];
+
+void order_update_list();
 
 
 #endif
